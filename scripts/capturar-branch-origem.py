d28c2e416753e85e600c9fa0222cc059f84e205e
deyvidksc <deyvidksc@gmail.com>
2024-12-11 22:38:03 -0300

    Update capturar-branch-origem.py

--- scripts/capturar-branch-origem.py
+++ scripts/capturar-branch-origem.py
@@ -1,30 +1,16 @@
 import os
-import subprocess
 import git
 
 def save_branch_name():
-    # Obter o nome da branch
-    branch_name = os.environ['GITHUB_HEAD_REF']
+    """Salva o nome da branch atual em um arquivo chamado branch-name.txt"""
 
-    # Criar o arquivo e escrever o nome da branch
+    # Obtém o nome da branch atual
+    repo = git.Repo('.')
+    branch_name = repo.active_branch.name
+
+    # Cria o arquivo e escreve o nome da branch
     with open('branch-name.txt', 'w') as f:
         f.write(branch_name)
 
-    # Inicializar o repositório Git
-    repo = git.Repo('.')
-
-    # Verificar se houve alterações e commitar/pushar
-    if repo.is_dirty():
-        repo.git.add('branch-name.txt')
-        repo.git.commit(m="Salvar nome da branch no arquivo")
-        repo.git.push('origin', 'main')
-    else:
-        print("Nenhuma alteração no arquivo branch-name.txt, nada para commit.")
-
-# Verificar se o arquivo existe antes de executar o script
-if not os.path.isfile('branch-name.txt'):
-    print("O arquivo branch-name.txt é necessário para continuar. Abortando.")
-    exit(1)
-
-# Executar a função principal
-save_branch_name()
+if __name__ == "__main__":
+    save_branch_name()
