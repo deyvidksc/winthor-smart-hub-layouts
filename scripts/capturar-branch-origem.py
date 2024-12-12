d25e25905b2cd51a30af3e363dc0cbac5b258ce7
deyvidksc <deyvidksc@gmail.com>
2024-12-11 23:23:13 -0300

    Update capturar-branch-origem.py

--- scripts/capturar-branch-origem.py
+++ scripts/capturar-branch-origem.py
@@ -4,19 +4,15 @@ import argparse
 
 def save_branch_name(branch_name):
     """Salva o nome da branch atual em um arquivo chamado branch-name.txt"""
-    # Obtém o nome da branch atual
-    #repo = git.Repo('.')
-    
-    #head = repo.head    
-    #branch_name = head.name
     print("branch_name " + branch_name)    
-
-#branch_name = repo.active_branch.name
-#print(f"A branch atual é: {branch_name}")
     
-    # Cria o arquivo e escreve o nome da branch
+    # Create the file with the branch name
     with open('branch-name.txt', 'w') as f:
-        f.write(branch_name)
+    f.write(branch_name)
+    
+    # Use Git commands (assuming you want to stage the file)
+    repo = git.Repo('.')
+    repo.index.add(['branch-name.txt'])
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Save branch name to a file')
