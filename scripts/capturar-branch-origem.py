29f52e02d01816fd58d176816e68452fdcd0fbf9
deyvidksc <deyvidksc@gmail.com>
2024-12-11 22:45:31 -0300

    Update capturar-branch-origem.py

--- scripts/capturar-branch-origem.py
+++ scripts/capturar-branch-origem.py
@@ -3,11 +3,13 @@ import git
 
 def save_branch_name():
     """Salva o nome da branch atual em um arquivo chamado branch-name.txt"""
-
+     print("acessou save_branch_name")
     # Obtém o nome da branch atual
     repo = git.Repo('.')
-    head = repo.head
+    print("repositório: "+repo)
+    head = repo.head    
     branch_name = head.name
+    print("branch_name: "+branch_name)
     # Cria o arquivo e escreve o nome da branch
     with open('branch-name.txt', 'w') as f:
         f.write(branch_name)
